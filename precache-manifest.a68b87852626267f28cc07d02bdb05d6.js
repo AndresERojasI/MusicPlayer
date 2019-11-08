bec3a485f7e2df63fdd46e90d65c1590179b021e
andresrojas <andres.rojas.isaza@gmail.com>
2019-11-07 20:13:58 -0500

    Updates

---
+++ precache-manifest.a68b87852626267f28cc07d02bdb05d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f446cbb2c5a866711dd19e0ddd2dfa4",
+    "url": "/MusicPlayer/index.html"
+  },
+  {
+    "revision": "2252aca8b39a3def7877",
+    "url": "/MusicPlayer/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c0311a74c0752932eca0",
+    "url": "/MusicPlayer/static/js/2.db81ba12.chunk.js"
+  },
+  {
+    "revision": "2252aca8b39a3def7877",
+    "url": "/MusicPlayer/static/js/main.67427474.chunk.js"
+  },
+  {
+    "revision": "1880a8ea342155d851e7",
+    "url": "/MusicPlayer/static/js/runtime-main.12024449.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/MusicPlayer/static/media/logo.25bf045c.svg"
+  }
+]);
