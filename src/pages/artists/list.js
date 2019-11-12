343c5ae6214b16570c3102b29d5d481cb5c45599
andresrojas <andres.rojas.isaza@gmail.com>
2019-11-12 08:46:00 -0500

    Added styles to list artists

--- src/pages/artists/list.js
+++ src/pages/artists/list.js
@@ -3,6 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loadArtists } from "../../actions/artist_actions";
 import textFormat from "../../utils/formatText";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import styles from "./list.scss";
 
 export class List extends Component {
   componentDidMount() {
@@ -10,9 +18,30 @@ export class List extends Component {
   }
 
   renderList = artists =>
-    artists.map(artist => {
-      return <div key={artist.id}>{textFormat(artist.name)}</div>;
-    });
+    artists.map(artist => (
+      <Grid item xs={3} key={artist.id}>
+        <Card className={styles.artist_list_item} >
+          <Link to={`/artists/${artist.id}`} style={{ textDecoration: "none" }}>
+            <CardActionArea>
+              <CardMedia
+                className={styles.artist_list_media}
+                image={artist.image}
+                title={artist.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {artist.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Genres:{" "}
+                  {artist.genres.map(genre => textFormat(genre)).join(", ")}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Link>
+        </Card>
+      </Grid>
+    ));
 
   render() {
     const { list, loading } = this.props;
@@ -21,7 +50,13 @@ export class List extends Component {
     }
 
     const loadArtists = this.renderList(list);
-    return <div>{loadArtists}</div>;
+    return (
+      <div className={styles.list}>
+        <Grid container spacing={4}>
+          {loadArtists}
+        </Grid>
+      </div>
+    );
   }
 }
 
